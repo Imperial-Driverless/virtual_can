28fc12876eba74df4c96231c1d349164825f203d
Jakub Michalski <j-michalski@outlook.com>
2022-07-11 11:40:35 +0100

    Simple launch file

---
+++ launch/virtual_can.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    node = Node(
+        package="virtual_can",
+        executable="virtual_can_node",
+        name="virtual_can",
+        parameters=[
+            # {"use_sim_time": True},
+            # {"can_debug": 0},
+            # {"simulate_can": 1},
+            # {"can_interface": "vcan0"},
+            # {"loop_rate": 100},
+            # {"rpm_limit": 100},
+            # {"max_acc": 5.0},
+            # {"max_braking": 5.0},
+            # {"cmd_timeout": 0.5}
+        ],
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(node)
+    return ld
